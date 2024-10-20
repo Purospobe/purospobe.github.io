6437600c3e4511d6ff5134281f1dae817970d099
Purospobe <105559314+Purospobe@users.noreply.github.com>
2024-10-20 15:55:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const message = document.createElement('p');
+    message.textContent = 'This is a message added by JavaScript.';
+    document.body.appendChild(message);
+});
